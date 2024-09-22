35cfcf2d10fcdb4a815ef9734bcb9becfbc8916c
ifg-dev <ivanfragosogar@outlook.es>
2024-09-22 12:56:12 +0200

    feat(api): create spotify provider handler

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,30 @@
+import NextAuth from "next-auth";
+import SpotifyProvider from "next-auth/providers/spotify";
+
+const handler = NextAuth({
+    providers: [
+        SpotifyProvider({
+            clientId: process.env.SPOTIFY_CLIENT_ID as string,
+            clientSecret: process.env.SPOTIFY_CLIENT_SECRET as string
+        }),
+    ],
+    callbacks: {
+        async jwt({ token, account, profile }) {
+            // Persist the OAuth access_token and or the user id to the token right after signin
+            if (account) {
+                token.accessToken = account.access_token;
+                token.id = profile.id;
+            }
+
+            return token;
+        },
+        async session({ session, token }) {
+            session.user.accessToken = token.accessToken;
+            session.user.id = token.id;
+            
+            return session;
+        }
+    }
+});
+
+export { handler as GET, handler as POST };
