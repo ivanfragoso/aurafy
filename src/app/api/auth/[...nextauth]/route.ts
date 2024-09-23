7392f19c0d7a044327f023e3667a2c0b94a4983c
ifg-dev <ivanfragosogar@outlook.es>
2024-09-23 14:21:27 +0200

    feat(next-auth): set refresh token rotation

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,28 +1,46 @@
 import NextAuth from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
+import axios from "axios"
+
+const clientId = process.env.SPOTIFY_CLIENT_ID as string
+const clientSecret = process.env.SPOTIFY_CLIENT_SECRET as string
 
 const handler = NextAuth({
     providers: [
         SpotifyProvider({
-            clientId: process.env.SPOTIFY_CLIENT_ID as string,
-            clientSecret: process.env.SPOTIFY_CLIENT_SECRET as string
+            clientId: clientId,
+            clientSecret: clientSecret,
+            authorization: {
+                params: {
+                    scope: "user-read-email user-read-private user-top-read"
+                }
+            }
         }),
     ],
     callbacks: {
-        async jwt({ token, account, profile }) {
-            // Persist the OAuth access_token and or the user id to the token right after signin
-            if (account) {
-                token.accessToken = account.access_token;
-                token.id = profile.id;
+        async jwt({ token, user, account }) {
+            if (account && user) {
+                return {
+                    accessToken: account.access_token,
+                    accessTokenExpires: account.expires_at,
+                    refreshToken: account.refresh_token
+                }
             }
 
-            return token;
+            if (Date.now() < token.accessTokenExpires) {
+                return token
+            }
+
+            return refreshAccessToken(token)
         },
+
         async session({ session, token }) {
-            session.user.accessToken = token.accessToken;
-            session.user.id = token.id;
-            
-            return session;
+            if (token) {
+                session.user = token.user
+                session.accessToken = token.accessToken
+            }
+        
+            return session
         }
     },
     pages: {
@@ -31,3 +49,34 @@ const handler = NextAuth({
 });
 
 export { handler as GET, handler as POST };
+
+async function refreshAccessToken(token) {
+    console.log(token)
+    const formData = new URLSearchParams({ grant_type: 'refresh_token', refresh_token: token.refreshToken });
+
+    try {
+        const response = await axios.post('https://accounts.spotify.com/api/token', formData.toString(), {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': 'Basic ' + Buffer.from(clientId + ':' + clientSecret).toString('base64')
+            }
+        });
+
+        const refreshedTokens = response.data;
+        console.log('Refreshed Tokens:', refreshedTokens);
+
+        return {
+            ...token,
+            accessToken: refreshedTokens.access_token,
+            accessTokenExpires: Date.now() + refreshedTokens.expires_at,
+            refreshToken: refreshedTokens.refresh_token ?? token.refreshToken
+        };
+    } catch (error) {
+        console.error('Error refreshing access token:', error.response ? error.response.data : error.message);
+        return {
+            ...token,
+            error: 'RefreshAccessTokenError'
+        };
+    }
+}
+
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <div className="flex justify-center items-center h-full">
-          {!isLoggedIn && <Button variant="outline" size="lg" onClick={handleLogin}>Get your Aura</Button>}
+          {!isLoggedIn && <Button variant="outline" size="lg" onClick={handleLogin}>Login</Button>}
           {isLoggedIn && <Button variant="outline" size="lg" onClick={() => signOut({ redirect: false })}>SignOut</Button>}
         </div>
       </main>
