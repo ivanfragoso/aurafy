8af340dd03d20876dbb005b035500fcf96f97d5c
ifg-dev <ivanfragosogar@outlook.es>
2024-09-27 12:51:02 +0200

    feat(home): create page ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,46 @@
+"use client"
+
+import { motion } from "framer-motion"
+import AuraBackground from "@/components/AuraBackground";
+import chroma from "chroma-js";
+import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
+import { signIn } from "next-auth/react";
+
 function Home() {
+  const heading = "Get your Aura based on your Spotify activity".split(" ")
+  const [colors, setColors] = useState<string[]>([]);
+
+  useEffect(() => {
+    const generatedColors = Array.from({ length: 20 }, () => chroma.random().hex());
+    setColors(generatedColors);
+  }, []);
 
   return (
-    <h1>Hello World</h1>
+    <>
+      <AuraBackground colors={colors}/>
+      <section className="h-full flex flex-col justify-center items-center relative z-20 gap-8">
+        <div className="text-center w-2/3">
+          <div className="inline">
+            {heading.map((el, i) => (
+              <motion.span
+                className="text-8xl font-bold"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }} 
+                transition={{
+                  duration: 1,
+                  delay: i / 8,
+                }}
+                key={i}
+              >
+                {el}{" "}
+              </motion.span>
+            ))}
+          </div>
+        </div>
+        <Button variant="secondary" onClick={() => signIn('spotify', {callbackUrl: "/aura"})} className="mt-4">Discover your Aura</Button>
+      </section>
+    </>
   );
 }
 
