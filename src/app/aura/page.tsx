27c95b373c8b5804c83605515d5f966814b95fa3
ifg-dev <ivanfragosogar@outlook.es>
2024-09-30 15:10:25 +0200

    feat(aura): implement animatepresence for loading fade in/out animations

--- src/app/aura/page.tsx
+++ src/app/aura/page.tsx
@@ -2,7 +2,7 @@
 
 import chroma from "chroma-js";
 import { useEffect, useState } from "react";
-import { motion, useMotionTemplate } from "framer-motion";
+import { AnimatePresence, motion, useMotionTemplate } from "framer-motion";
 import axios from "axios";
 import AuraCard from "@/components/AuraCard";
 import FloatingText from "@/components/FloatingText";
@@ -11,14 +11,16 @@ import AuraBackground from "@/components/AuraBackground";
 export default function Aura() {
     const [type, setType] = useState('')
     const [aura, setAura] = useState('')
-    const [names, setNames] = useState([])
+    const [names, setNames] = useState<string[]>([])
     const [gradientColors, setGradientColors] = useState<string[]>([])
     const [backgroundColor, setBackgroundColor] = useState<string[]>([])
+    const [loading, setLoading] = useState(false)
 
     const radialBackgroundImage = useMotionTemplate`radial-gradient(circle, ${gradientColors[0]} 10%, ${gradientColors[1]} 40%, ${gradientColors[2]} 70%)`
 
     const getAura = async (type: string) => {
         try {
+            setLoading(true)
             const result = await axios.get('/api/aura', {
                 params: { type }
             });
@@ -31,6 +33,8 @@ export default function Aura() {
             }
         } catch (error) {
             console.error("Error getting aura", error); // TODO: ui friendly error
+        } finally {
+            setLoading(false)
         }
     };
 
@@ -73,8 +77,11 @@ export default function Aura() {
     return (
         <section className="h-full" style={{backgroundColor: "#0a0a0a"}}>
             <AuraBackground colors={backgroundColor} color={aura}/>
+
+            <AnimatePresence mode="wait">
             {aura ? (
                 <div className="flex justify-center h-full items-center">
+
                     {/* Glowing sphere overlay */}
                     <motion.div
                         className="absolute z-10"
@@ -85,23 +92,19 @@ export default function Aura() {
                             borderRadius: "50%",
                             filter: "blur(30px)",
                         }}
-                        initial={{ scale: 0.5 }}
+                        initial={{ opacity: 0, scale: 0.5 }}
                         animate={{
+                            opacity: 1,
                             scale: [0.9, 1, 0.9],
-                        }}   
+                        }}
                         transition={{
-                            duration: 3,
-                            ease: "easeInOut",
-                            repeat: Infinity,
-                            repeatType: "loop",
+                            opacity: { duration: 1, ease: "easeInOut" },
+                            scale: { duration: 3, ease: "easeInOut", repeat: Infinity, repeatType: "loop" },
                         }}
                     >
                     </motion.div>
 
-                    {/* Aura text inside the sphere */}
-                    <span className="text-white text-lg relative z-10">{aura}</span> 
-
-                    {/*
+                    {/*}
                     {names.map((name, index) => {
                         const position = {
                         left: Math.random() * (100 - 10),
@@ -112,17 +115,44 @@ export default function Aura() {
                     */}
                 </div>
             ) : (
-                <div className="h-full px-4 flex gap-4 flex-col justify-center items-center md:flex-row">
-                    {auraCards.map((auraCard, index) => (
-                        <AuraCard
-                            key={index}
-                            title={auraCard.title}
-                            description={auraCard.description}
-                            btnClick={() => setType(auraCard.type)}
-                        />
-                    ))}
-                </div>
+                <motion.div 
+                    key={"loading"}
+                    className="h-full flex justify-center items-center"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    transition={{ duration: 1 }}
+                >
+                    <motion.div
+                        className="flex gap-4 justify-center items-center"
+                        initial={{ opacity: 1 }}
+                        animate={{ opacity: loading ? 0 : 1 }}
+                        transition={{ duration: 0.5 }}
+                    >
+                        {auraCards.map((auraCard, index) => (
+                            <AuraCard
+                                key={index}
+                                title={auraCard.title}
+                                description={auraCard.description}
+                                btnClick={() => setType(auraCard.type)}
+                            />
+                        ))}
+                    </motion.div>
+
+                    {loading && (
+                        <motion.div
+                            className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-xl z-10"
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            transition={{ delay: 0.5, duration: 1 }}
+                        >
+                            <span>Fetching your aura...</span>
+                        </motion.div>
+                    )}
+                </motion.div>
             )}
+
+            </AnimatePresence>
         </section>
     );
 }
