882767c8b9628b6725a289c47ca9fe5241b835c1
ifg-dev <ivanfragosogar@outlook.es>
2024-09-30 15:17:42 +0200

    feat(header): redirect to home on logo click

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,14 +3,15 @@
 import { signIn, useSession } from "next-auth/react"
 import { signOut } from "next-auth/react"
 import { Button } from "./ui/button"
-import Image from "next/image"
+import { useRouter } from "next/navigation"
 
 export default function Header() {
         const { data: session, status } = useSession()
+        const router = useRouter()
 
         return (
-            <header className="container fixed left-0 right-0 mx-auto flex justify-between items-center bg-transparent p-4 z-20">
-                <span className="text-2xl font-bold">aurafy</span>
+            <header className="container fixed left-0 right-0 mx-auto flex justify-between items-center bg-transparent p-4 z-30">
+                <span className="text-2xl font-bold cursor-pointer" onClick={() => router.push("/")}>aurafy</span>
 
                 { session && (
                     <>
