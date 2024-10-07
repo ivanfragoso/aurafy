1ef1fd33355fa3310a53ad5fdb71fcd7bcfae956
ifg-dev <ivanfragosogar@outlook.es>
2024-10-07 12:11:43 +0200

    feat(header): add sparkles to header logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,20 @@
 "use client"
 
-import { signIn, useSession } from "next-auth/react"
+import { useSession } from "next-auth/react"
 import { signOut } from "next-auth/react"
 import { Button } from "./ui/button"
 import { useRouter } from "next/navigation"
+import SparklesText from "./ui/sparkles-text"
+import chroma from "chroma-js"
 
 export default function Header() {
         const { data: session, status } = useSession()
         const router = useRouter()
+        const colors = {first: chroma.random().hex(), second: chroma.random().hex()}
 
         return (
             <header className="container fixed left-0 right-0 mx-auto flex justify-between items-center bg-transparent p-4 z-30">
-                <span className="text-2xl font-bold cursor-pointer" onClick={() => router.push("/")}>aurafy</span>
+                <SparklesText className="text-2xl cursor-pointer" onClick={() => router.push("/")} text="aurafy" sparklesCount={5} colors={colors}></SparklesText>
 
                 { session && (
                     <>
