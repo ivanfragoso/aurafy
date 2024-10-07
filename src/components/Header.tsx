e1e3a984e34a7acf8a322dbf7e5d9a80c97c1c7b
ifg-dev <ivanfragosogar@outlook.es>
2024-10-07 12:26:11 +0200

    fix(header): ensure consistency for sparkles generated colors

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,11 +6,19 @@ import { Button } from "./ui/button"
 import { useRouter } from "next/navigation"
 import SparklesText from "./ui/sparkles-text"
 import chroma from "chroma-js"
+import { useEffect, useState } from "react"
 
 export default function Header() {
         const { data: session, status } = useSession()
         const router = useRouter()
-        const colors = {first: chroma.random().hex(), second: chroma.random().hex()}
+        const [colors, setColors] = useState({ first: "#000000", second: "#000000" })
+
+        useEffect(() => {
+            const firstColor = chroma.random().hex()
+            const secondColor = chroma.random().hex()
+
+            setColors({ first: firstColor, second: secondColor })
+        }, [])
 
         return (
             <header className="container fixed left-0 right-0 mx-auto flex justify-between items-center bg-transparent p-4 z-30">
