d81191647175d4b9ab293506ebef88c5fc26b220
ifg-dev <ivanfragosogar@outlook.es>
2024-11-03 13:23:00 +0100

    feat(AuraBackground): set color transitions more smoothly

--- src/components/AuraBackground.tsx
+++ src/components/AuraBackground.tsx
@@ -1,5 +1,5 @@
 import { animate, motion, useMotionTemplate, useMotionValue } from "framer-motion";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import Particles from "./ui/particles";
 
 export interface AuraBackgroundProps {
@@ -8,27 +8,46 @@ export interface AuraBackgroundProps {
 }
 
 export default function AuraBackground({ color, colors }: AuraBackgroundProps) {
-    const motionColor = useMotionValue(colors[0]);
+    const motionColor = useMotionValue(colors[0]); // Iniciar con el primer color
     const backgroundImage = useMotionTemplate`radial-gradient(150% 175% at 50% 0%, #0a0a0a 50%, ${motionColor})`;
+    const colorIndex = useRef(0);
+    const intervalRef = useRef<NodeJS.Timeout | null>(null);
 
     useEffect(() => {
-        animate(motionColor, color || colors[0], {
-            ease: "easeInOut",
-            duration: 2,
-        });
-    }, [color, colors, motionColor]);
-
-    useEffect(() => {
-        if (!color) {
-            const animation = animate(motionColor, colors, {
+        if (color) {
+            animate(motionColor, color, {
                 ease: "easeInOut",
-                duration: 20,
-                repeat: Infinity,
-                repeatType: "mirror",
+                duration: 2,
             });
-            return () => animation.stop();
+
+            if (intervalRef.current) {
+                clearInterval(intervalRef.current);
+                intervalRef.current = null;
+            }
+        } else if (colors.length > 0) {
+            if (!intervalRef.current) {
+                animate(motionColor, colors[0], {
+                    ease: "easeInOut",
+                    duration: 2,
+                });
+
+                intervalRef.current = setInterval(() => {
+                    const nextColorIndex = (colorIndex.current + 1) % colors.length;
+                    animate(motionColor, colors[nextColorIndex], {
+                        ease: "easeInOut",
+                        duration: 2,
+                    });
+                    colorIndex.current = nextColorIndex;
+                }, 2000);
+            }
         }
-    }, [colors, motionColor, color]);
+
+        return () => {
+            if (intervalRef.current) {
+                clearInterval(intervalRef.current);
+            }
+        };
+    }, [color, colors, motionColor]);
 
     return (
         <>
